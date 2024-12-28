dddb12456b4d34126a3e16dcd81f12b7c2ab2ab6
sudipta_57 <ghoramiswapna32@gmail.com>
2024-12-28 13:02:08 +0530

    first commit

---
+++ news/src/App.jsx
@@ -0,0 +1,42 @@
+import AfterBanner from "./components/AfterBanner";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import NewPosts from "./components/NewPosts";
+import PopularPosts from "./components/PopularPosts";
+import TopBanner from "./components/TopBanner";
+import TopPosts from "./components/TopPosts";
+import TrendyPosts from "./components/TrendyPosts";
+import VideoSlider from "./components/VideoSlider";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* <TopBanner /> */}
+      {/* <AfterBanner /> */}
+      <img
+        src="/landing/Frame 16.png"
+        alt="landing"
+        className="object-cover w-full h-auto"
+      />
+      <PopularPosts />
+      <img
+        src="/landing/sports-widget.png"
+        alt="sports-widget"
+        className="object-cover w-full h-auto"
+      />
+      <NewPosts />
+      <VideoSlider />
+      <TrendyPosts />
+      <img
+        src="/landing/weather.png"
+        alt="weather"
+        className="object-cover w-full h-auto"
+      />
+      <TopPosts />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
